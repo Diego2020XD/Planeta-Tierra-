2932a3c2a54f554769a0238b4978c1c8b756cfc7
Diego2020XD <85714416+Diego2020XD@users.noreply.github.com>
2021-07-06 22:09:31 -0600

    Add files via upload

---
+++ Planeta Tierra/js/app.js
@@ -0,0 +1,74 @@
+var sol = document.getElementById("sol");
+
+
+if (moment().format("hh") >= 1 && moment().format("mm") == 00 || moment().format("hh") >= 1  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "100px"
+
+}
+
+if (moment().format("hh") >= 2 && moment().format("mm") == 00 || moment().format("hh") >= 2  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "200px"
+
+}
+
+if (moment().format("hh") >= 3 && moment().format("mm") == 00 || moment().format("hh") >= 3  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "300px"
+
+}
+
+if (moment().format("hh") >= 4 && moment().format("mm") == 00 || moment().format("hh") >= 4  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "400px"
+
+}
+
+if (moment().format("hh") >= 5 && moment().format("mm") == 00 || moment().format("hh") <= 5  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "500px"
+
+}
+
+if (moment().format("hh") >= 6 && moment().format("mm") == 00 || moment().format("hh") >= 6  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "600px"
+
+}
+
+if (moment().format("hh") >= 7 && moment().format("mm") == 00 || moment().format("hh") >= 7  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "700px"
+
+}
+
+if (moment().format("hh") >= 8 && moment().format("mm") == 00 || moment().format("hh") >= 8  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "800px"
+
+}
+
+if (moment().format("hh") >= 9 && moment().format("mm") == 00 || moment().format("hh") >= 9  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "900px"
+
+}
+
+if (moment().format("hh") >= 10 && moment().format("mm") == 00 || moment().format("hh") >= 10  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "1100px"
+
+}
+
+if (moment().format("hh") >= 11 && moment().format("mm") == 00 || moment().format("hh") >= 11  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "1200px"
+
+}
+
+if (moment().format("hh") >= 12 && moment().format("mm") == 00 || moment().format("hh") >= 12  && moment().format("mm") <= 59) {
+ 
+    sol.style.marginLeft = "1400px"
+
+}
